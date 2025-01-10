937d24db46e8b7c23f2ef32b74d995d0d5406c37
Ojiner <dbstjdrud115@gmail.com>
2025-01-10 10:40:23 +0900

    new start

---
+++ src/main/java/com/ll/chatApp/domain/article/article/service/ArticleService.java
@@ -0,0 +1,56 @@
+package com.ll.chatApp.domain.article.article.service;
+
+import com.ll.chatApp.domain.article.article.entity.Article;
+import com.ll.chatApp.domain.article.article.repository.ArticleRepository;
+import com.ll.chatApp.domain.article.articleComment.entity.ArticleComment;
+import com.ll.chatApp.domain.member.member.entity.Member;
+import com.ll.chatApp.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public RsData<Article> write(Long memberId, String title, String content) {
+        Article article = Article.builder()
+                .author(Member.builder().id(memberId).build())
+                .title(title)
+                .content(content)
+                .build();
+
+        articleRepository.save(article);
+
+        return RsData.of("200", "글 작성 성공", article);
+    }
+
+    public Optional<Article> findById(Long id) {
+        return articleRepository.findById(id);
+    }
+
+    @Transactional
+    public void modify(Article article, String title, String content) {
+        article.setTitle(title);
+        article.setContent(content);
+    }
+
+    @Transactional
+    public void modifyComment(ArticleComment comment, String commentBody) {
+        comment.setBody(commentBody);
+    }
+
+    public List<Article> findAll() {
+        return articleRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        this.articleRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/ll/chatApp/domain/member/member/entity/Member.java
@@ -0,0 +1,17 @@
+package com.ll.chatApp.domain.member.member.entity;
+
+import com.ll.chatApp.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class Member extends BaseEntity {
+    String username;
+    String password;
+}
